a76e41eb61582b539f89002a8b5f110a52a862f9
daiao <2476348804@qq.com>
2020-01-12 18:19:48 +0800

    本地第一次提交

---
+++ src/main/java/com/text/Helloword.java
@@ -0,0 +1,15 @@
+package com.text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* 说明这是一个SPRINGBOOT的主程序
+* */
+@SpringBootApplication
+public class Helloword {
+    public static void main(String[] args) {
+        //Spring应用启动起来
+     SpringApplication.run(Helloword.class,args);
+    }
+}
